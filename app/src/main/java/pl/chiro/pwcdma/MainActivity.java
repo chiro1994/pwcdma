ecbc9d2cd23f08de595de6a563d0d9a4ccd6003b
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2018-09-14 16:36:47 +0200

    Logowanie

--- app/src/main/java/pl/chiro/pwcdma/MainActivity.java
+++ app/src/main/java/pl/chiro/pwcdma/MainActivity.java
@@ -1,13 +1,30 @@
 package pl.chiro.pwcdma;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        } else {
+            username = firebaseUser.getDisplayName();
+        }
     }
 }
